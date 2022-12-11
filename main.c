9c4f915995d98d2893e27ac76516e7f23c721197
Johnathan Warlick <johnathanwarlick@gmail.com>
2022-12-10 23:09:40 -0600

    Begin

---
+++ main.c
@@ -0,0 +1,9 @@
+
+    #include <stdio.h>
+    int main()
+    {
+       printf("Welcome to Raven and Johnathan's 'Learning C' Adventure.\n");
+       printf("It is very dark in here.\n");
+       printf("\nBye!\n");
+       return 0;
+    }
